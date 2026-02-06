94d57197e99f6112ee049d60b6e5d67aaa2f228d
Vijayabhaskar <141475400+vijayabhaskarvb@users.noreply.github.com>
2026-02-07 01:25:48 +0530

    Update HuggingFaceEmbeddings import and model name

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from typing import List
 from langchain.schema import Document
 
@@ -45,5 +45,6 @@ def text_split(extracted_data):
 
 #Download the Embeddings from HuggingFace 
 def download_hugging_face_embeddings():
-    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')  #this model return 384 dimensions
-    return embeddings
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-MiniLM-L3-v2"
+    )
