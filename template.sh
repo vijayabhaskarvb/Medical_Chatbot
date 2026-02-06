809d1c4ae025ea901b9fff3fde64046ccea5e92f
Vijayabhaskar <141475400+vijayabhaskarvb@users.noreply.github.com>
2026-02-07 00:44:07 +0530

    Add files via upload

---
+++ template.sh
@@ -0,0 +1,14 @@
+#Creating Directories
+mkdir -p src
+mkdir -p research
+
+#Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+
+echo "Directory and files created Successfully!."
